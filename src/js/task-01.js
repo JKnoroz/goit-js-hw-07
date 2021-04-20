1f648983e5551f8e5cea6239c7d5c3ff3d966055
Yuliya <jknoroz@gmail.com>
2021-04-20 23:48:50 +0300

    task1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,13 @@
-const navCat = document.querySelector(".item");
-console.log(navCat);
-console.log(navCat.textContent);
+const navCat = document.querySelectorAll(".item");
+// console.log(navCat);
+const navCatLength = navCat.length;
+console.log(`В списке ${navCatLength} категории`);
+
+navCat.forEach((item) =>
+  console.log(
+    "Категория:",
+    item.firstElementChild.textContent,
+    ", Количество элементов:",
+    item.lastElementChild.childElementCount
+  )
+);
