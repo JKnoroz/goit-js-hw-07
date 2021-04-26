900fe99f33f2da57e0598c12626f10084df0d37d
Yuliya <jknoroz@gmail.com>
2021-04-26 23:13:33 +0300

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,2 +1,18 @@
 const counterValue = document.querySelector("#value");
-console.log(counterValue);
+const buttons = document.querySelectorAll("#counter button");
+
+const decrementBtn = buttons[0];
+const incrementBtn = buttons[1];
+
+const step = 1;
+let calcValue = 0;
+
+decrementBtn.addEventListener("click", () => {
+  calcValue -= step;
+  counterValue.innerHTML = calcValue;
+});
+
+incrementBtn.addEventListener("click", () => {
+  calcValue += step;
+  counterValue.innerHTML = calcValue;
+});
