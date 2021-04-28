f6d88365f475e96ce4f80c6ebc06c4cddf6d51ed
Yuliya <jknoroz@gmail.com>
2021-04-29 00:04:50 +0300

    task6

---
+++ src/js/task-06.js
@@ -0,0 +1,13 @@
+const input = document.getElementById("validation-input");
+const validLength = Number(input.getAttribute("data-length"));
+
+input.addEventListener("blur", onInputBlurLengthValidator);
+
+function onInputBlurLengthValidator(event) {
+  if (event.currentTarget.value.length !== validLength) {
+    input.classList.add("invalid");
+  } else {
+    input.classList.add("valid");
+    input.classList.remove("invalid");
+  }
+}
